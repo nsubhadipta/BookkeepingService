fe4ac32f5b1924ab4583057328ef75902fccaa86
Subhadipta Nayak <nsubhadipta@gmail.com>
2024-07-23 16:48:40 +0530

    user register and login request valiadtion scheme

---
+++ src/utils/RequestValidators/user.scheme.js
@@ -0,0 +1,18 @@
+const Joi = require("joi");
+
+const userSchema = {
+  register: Joi.object().keys({
+    name: Joi.string().max(50).required(),
+    email: Joi.string().max(150).required(),
+    role: Joi.string().valid("Author", "Borrower").required(),
+    password: Joi.string().max(50).required(),
+    description: Joi.string().allow(null, "").optional(),
+  }),
+
+  login: Joi.object().keys({
+    email: Joi.string().max(150).required(),
+    password: Joi.string().max(50).required(),
+  }),
+
+};
+module.exports = userSchema;
