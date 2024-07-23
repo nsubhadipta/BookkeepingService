38c68a229f15b622d03fed6df486696321f97f44
Subhadipta Nayak <nsubhadipta@gmail.com>
2024-07-23 16:00:33 +0530

    creating a middleware to sanitise the input request using joi validation

---
+++ src/middlewares/validationMiddleware.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+const middleware = (schema, property) => {
+    return (req, res, next) => {
+        const {
+            error
+        } = schema.validate(req.body);
+        const valid = error == null;
+
+        if (valid) {
+            next();
+        } else {
+            const {
+                details
+            } = error;
+            const message = details.map(i => i.message).join(',');
+
+            //console.log("error", message);
+            res.status(422).json({
+                error: message,
+                status: 0
+            })
+        }
+    }
+}
+module.exports = middleware;
