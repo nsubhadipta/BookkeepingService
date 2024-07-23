2eef829e93e0c328eb92769902196dbf35badd65
Subhadipta Nayak <nsubhadipta@gmail.com>
2024-07-23 15:12:30 +0530

    book model

---
+++ src/models/bookModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: { type: String, default: "" },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  library: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Library",
+    required: true,
+  },
+  borrower: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  borrowLogs: [
+    {
+      borrowed: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      charge: {
+        type: String,
+        required: true,
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  coverImage: {
+    type: String,
+    required: false,
+  },
+  active: { type: Boolean, required: false, default: true },
+},
+{
+  timestamps: true, versionKey: false
+}
+);
+
+module.exports = mongoose.model('Book', bookSchema, 'Book');
